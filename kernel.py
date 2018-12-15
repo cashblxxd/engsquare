20ec3f3ee53a5f38964aff558878b2e3724d372b
Dev 0 <0xff@ya.ru>
2018-12-16 00:12:38 +0300

    Pre-release. (patch 1.2.0) # TODO: settings, speech kit, more words 4 game

--- kernel.py
+++ kernel.py
@@ -4,28 +4,30 @@ from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow
 import PyQt5.QtGui as QtGui
 from PyQt5 import uic
 import os
+from scripts.engsquare import Ui_StartWIndow
 
-
-class StartWindow(QMainWindow):
+class StartWindow(QMainWindow, Ui_StartWIndow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('sources/engsquare.ui', self)
+        self.setupUi(self)
         self.vocabulary, self.count = {}, 0
         self.pushButton.clicked.connect(self.run_workout)
         self.pushButton_2.clicked.connect(self.run_vocabulary)
-        #self.pushButton_3.clicked.connect(self.run_guide)
+        self.pushButton_3.clicked.connect(self.run_game)
         #self.pushButton_4.clicked.connect(self.run_settings)
 
     def run_workout(self):
-        os.system("python3 sources/workout.py")
-
+        os.system("python3 scripts/workout.py")
 
     def run_vocabulary(self):
-        os.system("python3 sources/vocabulary.py")
+        os.system("python3 scripts/vocabulary.py")
+
+    def run_game(self):
+        os.system("python3 scripts/game.py")
 
 
-    def run_settings(self):
-        os.system("python3 settings.py")
+    #def run_settings(self):
+    #    os.system("python3 settings.py")
 
 
 if __name__ == '__main__':
